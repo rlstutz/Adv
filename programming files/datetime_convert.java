3342c713152f5c6a49ec168602b21a56ea975042
cooperdiego <cldiego@uvm.edu>
2026-04-07 11:12:41 -0400

    Fixed the issue, it was .format instead of .parse. Also public static void was just public static

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -2,10 +2,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
-    public static main(String[] args) {
+    public static void main(String[] args) {
         String dateStr = "2022-03-17 10:45:30";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateObj = LocalDateTime.format(dateStr, formatter);
+        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         System.out.println(formattedDate);
