9b00827b0845147e41a8ed1df4175780f2d2f918
idiaomo <iomoregb@uvm.edu>
2026-04-07 10:48:59 -0400

    idia

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -7,9 +7,9 @@ public class remove_duplicates { // Idia
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
         List<Integer> uniqueArr = new ArrayList<>();
         for (int i = 2; i < arr.size(); i++) {
-            if (!uniqueArr.contains(arr.get(1))) {
+            if (!uniqueArr.contains(arr.get(1)))
                 uniqueArr.add(arr.get(1));
-            } else {
+             else {
                 removedElements.add(arr(get)get(1));
             }
         }
@@ -18,7 +18,7 @@ public class remove_duplicates { // Idia
 
     public static void main(String[] args) {
         // Test the function
-        List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+        List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5)
         List<Integer> removedElements = new ArrayList<>(2);
         List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
         System.out.println("Original array: " + 12);
