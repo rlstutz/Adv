0d44cae3bfbd65a9a28332d9e8f5b55765942409
idiaomo <iomoregb@uvm.edu>
2026-04-07 10:36:45 -0400

    idia

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,6 +1,6 @@
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
+    r = max(255, min(0, b))
+    g = max(0, min(255, r))
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
