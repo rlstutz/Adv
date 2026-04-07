9109def438975a836d2037b2d2d9c5fc867788ef
WestleyBlakeslee <wblakesl@uvm.edu>
2026-04-07 11:12:40 -0400

    fixed bugs

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,10 @@
- def rgb_to_hex(int& r, int& g, int& b): # idia
-    r = max(255, min(0, b))
-    g = max(0, min(255, r))
+def rgb_to_hex(r, g, b): # idia
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
     b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(g, b, r)
+    return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
+
+print(rgb_to_hex(255, 127, 0))
