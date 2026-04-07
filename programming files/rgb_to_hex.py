e4c2a991dc0db2d43a80399e089dc786b9e1cafd
idiaomo <iomoregb@uvm.edu>
2026-04-07 10:29:43 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -3,6 +3,6 @@ def rgb_to_hex(r, g, b):
     g = max(0, min(255, g))
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
-
+// idiaomo 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
