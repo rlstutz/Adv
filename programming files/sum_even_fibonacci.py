b369ed3197128db6766fe28608e6eab381cfa6f6
lex120 <aberman120@gmail.com>
2026-04-07 10:44:00 -0400

    broke things

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,12 @@
-a, b = 0, 1
-sum_even = 0
+a, b = 0, 0
+sum_even = 1
 while b < 4000000:
     if b % 2 == 0:
-        sum_even += b
+        sum_even_haha += b
     a, b = b, a+b
 
 print(sum_even)
 
 # answer should be 4613732
+
+# AB
